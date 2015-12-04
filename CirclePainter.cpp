668dab02588666a6a3e77a56202ac773f8089688
HoShuHang <kwite2002@gmail.com>
2015-12-04 12:32:17 +0800

    modify circlepainter

--- CirclePainter.cpp
+++ CirclePainter.cpp
@@ -1,5 +1,7 @@
 #include "CirclePainter.h"
 
+#include <iostream>
+using namespace std;
 CirclePainter::CirclePainter( qreal cx, qreal cy, qreal r, QWidget *_widget)
 {
     //ctor
@@ -16,7 +18,14 @@ CirclePainter::~CirclePainter()
 
 QRectF CirclePainter::boundingRect() const
 {
-    return QRectF(centerX,centerY,radius,radius);
+    return QRectF(centerX-radius, centerY-radius, 2*radius, 2*radius);
+}
+
+QPainterPath CirclePainter::shape() const
+{
+    QPainterPath path;
+    path.addEllipse(boundingRect());
+    return path;
 }
 
 void CirclePainter::paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget)
@@ -34,4 +43,39 @@ void CirclePainter::paint(QPainter * painter, const QStyleOptionGraphicsItem * o
 void CirclePainter::SetComposite(bool isC)
 {
     isComposite = isC;
+}
+
+void CirclePainter::hoverEnterEvent ( QGraphicsSceneHoverEvent * event )
+{
+    cout << "hoverEnterEvent : 41" << endl;
+}
+
+void CirclePainter::hoverLeaveEvent ( QGraphicsSceneHoverEvent * event )
+{
+cout << "hoverLeaveEvent : 46" << endl;
+}
+
+void CirclePainter::mouseMoveEvent ( QGraphicsSceneMouseEvent * event )
+{
+cout << "mouseMoveEvent : 51" << endl;
+}
+
+void CirclePainter::mouseMoveEvent(QGraphicsSceneDragDropEvent *event)
+{
+cout << "mouseMoveEvent : 56" << endl;
+}
+
+void CirclePainter::mousePressEvent (QGraphicsSceneMouseEvent * event )
+{
+cout << "mousePressEvent : 61" << endl;
+}
+
+void CirclePainter::mousePressEvent(QGraphicsSceneDragDropEvent *event)
+{
+cout << "mousePressEvent : 66" << endl;
+}
+
+void CirclePainter::mouseReleaseEvent (QGraphicsSceneMouseEvent * event )
+{
+cout << "mouseReleaseEvent : 71" << endl;
 }
--- CirclePainter.h
+++ CirclePainter.h
@@ -5,20 +5,29 @@
 #include <QPainter>
 class CirclePainter:public QGraphicsItem
 {
-    public:
-        CirclePainter( qreal cx, qreal cy, qreal r, QWidget *_widget);
-        virtual ~CirclePainter();
+public:
+    CirclePainter( qreal cx, qreal cy, qreal r, QWidget *_widget);
+    virtual ~CirclePainter();
 
-        QWidget *widget;
-        const QRectF *rectptr;
-        qreal centerX;
-        qreal centerY;
-        qreal radius;
-        QRectF boundingRect() const;//必須實做的virtual function，但目前用不到
-        void paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget);
-        void SetComposite(bool isC);
-    private:
-        bool isComposite = false;
+    QWidget *widget;
+    const QRectF *rectptr;
+    qreal centerX;
+    qreal centerY;
+    qreal radius;
+    QRectF boundingRect() const;//必須實做的virtual function，但目前用不到
+    QPainterPath shape() const;
+    void paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget);
+    void SetComposite(bool isC);
+protected:
+    virtual void hoverEnterEvent ( QGraphicsSceneHoverEvent * event );
+    virtual void hoverLeaveEvent ( QGraphicsSceneHoverEvent * event );
+    virtual void mouseMoveEvent ( QGraphicsSceneMouseEvent * event );
+    virtual void mouseMoveEvent(QGraphicsSceneDragDropEvent *event);
+    virtual void mousePressEvent (QGraphicsSceneMouseEvent * event );
+    virtual void mousePressEvent(QGraphicsSceneDragDropEvent *event);
+    virtual void mouseReleaseEvent (QGraphicsSceneMouseEvent * event );
+private:
+    bool isComposite = false;
 };
 
 #endif // CIRCLEPAINTER_H_INCLUDED
