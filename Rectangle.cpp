c1ba2789a623a71e331cba64b688622e9be3492d
HoShuHang <kwite2002@gmail.com>
2015-12-18 18:26:33 +0800

    modify include

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,7 +1,6 @@
 #include "Rectangle.h"
 #include <sstream>
 #include <iostream>
-#include "RectanglePainter.h"
 
 Rectangle::Rectangle(int ll_x, int ll_y, int length, int width)
     :x(ll_x),y(ll_y),l(length),w(width)
